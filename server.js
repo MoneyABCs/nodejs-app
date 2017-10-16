7717412fc5e2600b9e74aad40cb24c20654a060d
MoneyABCs <utaflpmoneyabcs@gmail.com>
2017-10-16 15:19:05 -0500

    Cron job should run every day in a week

--- server.js
+++ server.js
@@ -354,7 +354,7 @@ var cronJob = function(){
     var schedule = require('node-schedule');
     //cron job
     var rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+    rule.dayOfWeek = new schedule.Range(0, 6);
     rule.hour = 04;
     rule.minute = 00;
     var j = schedule.scheduleJob(rule, function(){
@@ -365,17 +365,31 @@ var cronJob = function(){
     });
 
 }
+
 cronJob();
 
-var schedule = require('node-schedule');
-var l = schedule.scheduleJob('2 * * * *',function(){
-    console.log("Log for every 2 min")
-});
+// var testcron = function(){
+//     var schedule = require('node-schedule');
+//     var rule = new schedule.RecurrenceRule();
+//     rule.dayOfWeek = new schedule.Range(0, 6);
+//     rule.hour = 14;
+//     rule.minute = 34;
+//     var j = schedule.scheduleJob(rule, function(){
+//         console.log('test cron');
+//     });
+//
+// };
+// testcron();
 
-var cron = require('node-cron');
-cron.schedule('1 * * * *',function(){
-    console.log("runnig evryr 1 min")
-});
+// var schedule = require('node-schedule');
+// var l = schedule.scheduleJob('2 * * * *',function(){
+//     console.log("Log for every 2 min")
+// });
+//
+// var cron = require('node-cron');
+// cron.schedule('1 * * * *',function(){
+//     console.log("runnig evryr 1 min")
+// });
 
 var GoogleSearch = require('google-search');
 var customsearch = google.customsearch('v1');
